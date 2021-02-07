3b65b5bb48ff7f10b7844c10641b145bdfa28b29
nicolasSAPIN <nicosapin@gmail.com>
2021-02-07 18:59:36 +0100

    first commit

---
+++ Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvNetTp1.Entities
+{
+    public class Author
+    {
+        private long id;
+        private string firstname;
+        private string lastname;
+
+        private List<Book> books = new List<Book>();
+    }
+}
---
+++ Entities/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvNetTp1.Entities
+{
+    public class Book
+    {
+        private long id;
+        private string name;
+        private int nb_page;
+
+
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(BookContract.USER_COLUMN_ID)]
+        public long Id { get => this.id; set => this.id = value; }
+
+        [Column(BookContract.USER_COLUMN_FIRSTNAME)]
+        [Required]
+        public string name { get => this.name; set => this.name = value; }
+
+        [Column(UserContract.USER_COLUMN_LASTNAME)]
+        [Required]
+        public string Lastname { get => this.lastname; set => this.lastname = value; }
+
+    }
+}
